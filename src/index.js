d6fc8cc018d7e1e86e6c53ef9b069d8b72b7746c
whaleism <230928187+whaleism@users.noreply.github.com>
2025-10-01 12:06:52 -0500

    Added function and moment in js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+function updateTime() {
+  // Lapland, Finland (use Europe/Mariehamn for timezone)
 
+  let laplandElement = document.querySelector("#lapland");
+  let laplandDateElement = laplandElement.querySelector(".date");
+  let laplandTimeElement = laplandElement.querySelector(".time");
+  let laplandTime = moment().tz("Europe/Mariehamn");
+
+  laplandDateElement.innerHTML = laplandTime.format("MMMM Do YYYY");
+  laplandTimeElement.innerHTML = laplandTime.format(
+    "h:mm:ss [<small>]A[</small]"
+  );
+
+  // Tokyo, Japan (use Asia/Tokyo for timezone)
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
